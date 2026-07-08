88e2a68dc9c375666801e6c94d5b4b57f93caaec
havividan4 <havividan4@gmail.com>
2026-07-08 20:41:04 +0300

    initializing project

---
+++ client/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600"],
+});
+
+export const metadata: Metadata = {
+  title: "Idan Haviv",
+  description: "Fullstack developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${poppins.className} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ client/components/icons/html.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+
+const HTML = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 81.125 81.125"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <rect width="81.125" height="81.125" fill="#EF652A" />
+    <path
+      d="M52.716 32.0322L53.1724 27.0396H27.9523L29.2932 42.2743H46.7533L46.1256 48.7791L40.5053 50.2912L34.9135 48.7791L34.5426 44.785H29.5499L30.1776 52.7162L40.5053 55.5691H40.6194V55.5406L50.8615 52.7162L52.288 37.1961H33.915L33.487 32.0322H52.716ZM20.4775 18.6519H60.6472L56.9954 59.6774L40.5053 64.2992L24.1293 59.6774L20.4775 18.6519Z"
+      fill="white"
+    />
+  </svg>
+);
+
+export default HTML;
