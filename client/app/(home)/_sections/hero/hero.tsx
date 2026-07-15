fc96b49f027fac9d1d3db92ae805052d12469dfe
havividan4 <havividan4@gmail.com>
2026-07-16 01:14:40 +0300

    adding bitmoji to hero section

--- client/app/(home)/_sections/hero/hero.tsx
+++ client/app/(home)/_sections/hero/hero.tsx
@@ -13,7 +13,7 @@ const Fact = dynamic(() => import("./fact"), {
 const Hero = () => {
   return (
     <div className="w-fit h-min grid grid-cols-[repeat(3,min-content)] grid-rows-[repeat(5,min-content)] gap-5">
-      <div className="w-full h-full flex flex-col gap-2">
+      <div className="group relative w-full h-full flex flex-col gap-2 rounded-lg overflow-hidden cursor-pointer">
         <h1>Idan Haviv</h1>
 
         <div className="flex items-center gap-2">
@@ -21,6 +21,11 @@ const Hero = () => {
 
           <p className="text-[1rem]">Available to work</p>
         </div>
+
+        <img
+          src="/assets/hero/hi.png"
+          className="absolute right-0 bottom-0 h-full aspect-square origin-bottom-right translate-full scale-135 transition duration-700 ease-out group-hover:translate-x-1/5 group-hover:translate-y-0 group-hover:-rotate-15"
+        />
       </div>
 
       <Age />
